78d313314a76d26ba3a46f53e1287d1f024c14c2
TrenTech@gmail.com <TrenTech@gmail.com>
2016-10-18 01:03:27 -0400

    flawed..so flawed

--- src/main/java/com/gmail/trentech/helpme/commands/elements/HelpElement.java
+++ src/main/java/com/gmail/trentech/helpme/commands/elements/HelpElement.java
@@ -27,7 +27,7 @@ public class HelpElement extends CommandElement {
         while (args.hasNext()) {
             ret.append(' ').append(args.next());
         }
-        String rawCommand = ret.toString().toLowerCase();
+        String rawCommand = ret.toString();
 
         Optional<Help> optionalHelp = Help.get(rawCommand);
 
@@ -39,21 +39,65 @@ public class HelpElement extends CommandElement {
     }
 
     @Override
-    public List<String> complete(CommandSource src, CommandArgs args, CommandContext context) {
+    public List<String> complete(CommandSource src, CommandArgs args, CommandContext context) {  	
     	List<String> list = new ArrayList<>();
 
-    	Optional<String> next = args.nextIfPresent();
-    	
-    	if(next.isPresent()) {
-            for(Help help : Help.getAll()) {
-            	if(help.getRawCommand().startsWith(next.get().toLowerCase())) {
-            		list.add(help.getRawCommand());
-            	}
-            }
+    	String next = args.getRaw();
+
+    	if(next.equals("")) {
+    		for(Help help : Help.getParents()) {   			
+    			list.add(help.getRawCommand());
+    		}
     	} else {
-            for(Help help : Help.getAll()) {
-            	list.add(help.getRawCommand());
-            }
+    		if(next.contains(" ")) { 			
+    			String[] split = next.split(" ");
+
+    			Optional<Help> optionalParent = Help.get(split[0]);
+    			
+    			if(optionalParent.isPresent()) {			
+    				Help parent = optionalParent.get();
+
+    				String command = parent.getCommand();
+    				
+        			for(int i = 1; i < split.length; i++) {
+        				command = command + " " + split[i];
+        				
+        				if(i == (split.length - 1)) {
+                    		for(Help help : parent.getChildren()) {
+                    			list.add(help.getCommand());
+                    		}
+                    		
+                    		return list;
+        				}
+        				
+                		for(Help help : parent.getChildren()) {
+                			if(help.getRawCommand().startsWith(command)) {
+                				if(help.getRawCommand().equals(command)) {
+                					if(i == (split.length - 1)) {
+	            						System.out.println("equals command, Last arg");
+	            						
+	                            		for(Help h : help.getChildren()) {
+	                            			list.add(h.getCommand());
+	                            		}
+	                				}
+                				} else if(i == (split.length - 1)) {
+                					list.add(help.getCommand());
+                				}
+                			}
+                			
+                			parent = help;
+                		}
+        			}
+    			}
+    		} else {
+    			System.out.println("No space");
+        		for(Help help : Help.getParents()) {
+        			if(help.getRawCommand().startsWith(next)) {
+        				list.add(help.getCommand());
+        			}
+        		}
+    		}
+
     	}
 
         return list;
@@ -62,5 +106,23 @@ public class HelpElement extends CommandElement {
     @Override
     public Text getUsage(CommandSource src) {
         return Text.of(getKey());
+    }
+    
+    public List<String> get(List<Help> list, String compare) {
+    	List<String> l = new ArrayList<>();
+    	
+        for(Help help : list) {
+        	if(help.getRawCommand().startsWith(compare)) {
+        		if(help.getRawCommand().equals(compare)) {
+        			if(help.getChildren().isEmpty()) {
+        				l.add(help.getCommand());
+        			} else {
+        				l.addAll(get(help.getChildren(), compare));
+        			}
+        		}
+        	}
+        }
+        
+        return l;
     }
 }
